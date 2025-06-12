7a3c61b4013accc3b0d54356f3fd109ff5b89e1b
hawk0120 <brady93hawkins@gmail.com>
2025-06-12 18:46:36 +0100

    updated icons

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,5 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home';
 import { ProjectsComponent } from './pages/projects/projects';
-import { ContactComponent } from './pages/contact/contact';
 
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home';
 import { ProjectsComponent } from './pages/projects/projects';
-import { ContactComponent } from './pages/contact/contact';
 
 export const routes: Routes = [
 { path: '', component: HomeComponent },
 { path: 'projects', component: ProjectsComponent },
-{ path: 'contact', component: ContactComponent }
 ];
 
 @NgModule({
