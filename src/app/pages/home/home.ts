591de245cd12bd5f813a1070a7086e46e8eb8412
Brady Hawkins <brady93hawkins@gmail.com>
2025-07-07 11:07:05 +0200

    Update home.ts

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common'
 
 @Component({
   standalone: true,
   selector: 'app-home',
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
